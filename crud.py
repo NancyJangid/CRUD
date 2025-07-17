bc24c57823bf42173c9805cb8c543922ab932f03
NancyJangid <75533310+NancyJangid@users.noreply.github.com>
2025-07-17 23:55:15 +0530

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,43 @@
+import pyodbc
+from fastapi import FastAPI
+import json
+
+database_connection ="DRIVER={SQL Server};SERVER=43.204.45.205,1560;DATABASE=HSRPOEM;UID=R3480;PWD=Yashwant@13"
+conn = pyodbc.connect(database_connection)
+cursor = conn.cursor()
+cursor.execute("Select * from Items")
+data = cursor.fetchall()
+columns = [desc[0] for desc in cursor.description]
+print(columns)
+print(data)
+
+lst = []
+dic={}
+for row in data:
+ 
+    for y in range (len(columns)):
+        dic[columns[y]] = row[y] 
+    lst.append(dic)
+print(lst)
+print(dic)
+
+cursor.close()
+conn.close()
+
+app = FastAPI()
+@app.get("/getdata")
+def GET():
+    conn = pyodbc.connect(database_connection)
+    cursor = conn.cursor()
+    cursor.execute("Select * from Items")
+    data = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    
+    dic = {}
+    for row in data:
+        for y in range (len(columns)):
+            dic[columns[y]] = row[y]  
+
+    records=json.dumps(dic, default=str)
+
+    return {records}
